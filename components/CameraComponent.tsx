45d636b002b1391a862cd5d6f6f008e0c76b2095
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2025-01-23 18:16:22 +0100

    fix: enhance CameraComponent to handle scanning errors with fallback image

--- components/CameraComponent.tsx
+++ components/CameraComponent.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import { View } from "react-native";
 import { CameraCapturedPicture } from "expo-camera";
-import DocumentScanner, {
-  ScanDocumentResponse,
-} from "react-native-document-scanner-plugin";
+import DocumentScanner from "react-native-document-scanner-plugin";
 import { router } from "expo-router";
+import { Secrets } from "@/Secrets";
 
 interface CameraComponentProps {
   onCapture: (photoUri: CameraCapturedPicture) => void;
@@ -20,8 +19,16 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ onCapture }) => {
         onCapture({ uri: scannedImages[0], width: 0, height: 0 });
       }
     } catch (error) {
-      console.error("Error scanning document:", error);
-      router.replace("/document");
+      if (Secrets.disableScan) {
+        onCapture({
+          uri: "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/thumbnails/image/file.jpg",
+          width: 0,
+          height: 0,
+        });
+      } else {
+        console.error("Error scanning document:", error);
+        router.replace("/document");
+      }
     }
   };
 
