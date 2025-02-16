129a2491548766998b189d305aea2f27d90c8a23
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2025-02-16 15:56:56 +0100

    fix: remove unused import from schema file

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,5 @@
 import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
 import { relations } from "drizzle-orm";
-import type { CameraCapturedPicture } from "expo-camera";
 
 // Main Document table
 export const documents = sqliteTable("documents", {
